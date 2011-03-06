d47d9d6869180550ad479deaa66e773c42cd8b08
Gael Ratovelo <gael.ratovelo@binomed.fr>
2011-03-06 17:23:06 +0100

    Login Form

---
+++ application/forms/UserSignin.php
@@ -0,0 +1,36 @@
+<?php
+
+class Application_Form_UserSignin extends Zend_Form
+{
+
+    public function init()
+    {
+        $this->setMethod('post');
+		$this->setName('signin');
+		
+		// Element Login
+		$login = new Zend_Form_Element_Text('login');
+		$login->setLabel('Login')
+			  ->setRequired(true)
+			  ->addFilter('StripTags')
+			  ->addFilter('StringTrim')
+			  ->addValidator('NotEmpty');
+			  
+		$password = new Zend_Form_Element_Password('password');
+		$password->setLabel('Password')
+				 ->setRequired(true)
+				 ->addFilter('StripTags')
+				 ->addFilter('StringTrim')
+				 ->addValidator('NotEmpty')
+				 ->addValidator('StringLength', false, array(6,15));
+		
+		$this->addElement($login)
+			 ->addElement($password)
+			 ->addElement('submit','submit', array (
+				'ignore'		=> true,
+				'label'			=> 'Signup'));
+    }
+
+
+}
+
