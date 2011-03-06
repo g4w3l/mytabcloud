25343910cb3332562a27524c48604eb6faea0255
Gael Ratovelo <gael.ratovelo@binomed.fr>
2011-03-06 11:35:37 +0100

    First Commit

---
+++ scripts/mytabcloud.sql
@@ -0,0 +1,14 @@
+-- MyTabCloud SQL Scripting
+-- Author : Gael Ratovelo
+
+CREATE TABLE IF NOT EXISTS `mtc_users` (
+    `usr_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `usr_login` VARCHAR( 50 ) NOT NULL ,
+    `usr_mail` VARCHAR( 100 ) NOT NULL ,
+    `usr_password` VARCHAR( 32 ) NULL ,
+    `usr_password_salt` VARCHAR( 32 ) NULL ,
+    `usr_name` VARCHAR( 50 ) NULL ,
+    UNIQUE (
+    `usr_login`
+    )
+) ENGINE = InnoDB;
