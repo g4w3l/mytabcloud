5e490cc2bbdbd53c998194bc6f1f94e15367d558
Gael Ratovelo <gael.ratovelo@binomed.fr>
2011-03-13 23:25:57 +0100

    Javascript

--- application/views/helpers/JavascriptHelper.php
+++ application/views/helpers/JavascriptHelper.php
@@ -5,8 +5,8 @@ class Zend_View_Helper_JavascriptHelper extends Zend_View_Helper_Abstract
         $request = Zend_Controller_Front::getInstance()->getRequest();
         $file_uri = '/mytabcloud/public/js/mytabcloud/' . $request->getControllerName() . '.js';
 
-        if (file_exists($file_uri)) {
+        //if (file_exists($file_uri)) {
             $this->view->headScript()->appendFile($file_uri);
-        }
+        //}
     }
 }
---
+++ public/js/mytabcloud/tab.js
@@ -0,0 +1,11 @@
+/**
+ * @author Gawel
+ */
+ 
+function bindKeyEvents() {
+	//$('input[name^="note"]').val('1');
+}
+
+window.onload = (function(){
+	bindKeyEvents();
+});
